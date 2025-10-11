0fd6d59d107f19a3965231d9ca894fbcb9de9285
ZilhaKalac <zilhakalac@gmail.com>
2025-10-11 22:59:14 +0300

    new changes are made

--- src/test/java/US201/deneme.java
+++
@@ -1,9 +0,0 @@
-package US201;
-
-public class deneme {
-    public static void main(String[] args) {
-        System.out.println("please god");
-        System.out.println("555");
-        System.out.println("vvv");
-    }
-}
