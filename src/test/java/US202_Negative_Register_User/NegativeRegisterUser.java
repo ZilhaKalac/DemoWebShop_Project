5f0d61fb3ff7137722ae5e60484baf45a8bef836
nldn24 <ahmetnaldan24@hotmail.com>
2025-10-13 13:13:11 +0300

    class places changed

--- src/test/java/US202_Negative_Register_User/NegativeRegisterUser.java
+++ src/test/java/US202_Negative_Register_User/NegativeRegisterUser.java
@@ -1,4 +1,7 @@
 package US202_Negative_Register_User;
 
 public class NegativeRegisterUser {
+    public static void main(String[] args) {
+        System.out.println("class places changed");
+    }
 }
