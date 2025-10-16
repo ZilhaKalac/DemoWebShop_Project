bca7af25c35ce8e8ff4b7263b33e85b83f6b0d2c
AliEnesAcikgoz <alienes.acikgoz1@gmail.com>
2025-10-16 15:45:35 +0300

    Logged_in_User update and Non_Logged_In_User update

---
+++ src/test/java/US207_Survey_Response/Non_Logged_In_User.java
@@ -0,0 +1,41 @@
+package US207_Survey_Response;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utility.BaseDriver;
+
+import java.time.Duration;
+
+public class Non_Logged_In_User {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = BaseDriver.driver("https://demowebshop.tricentis.com/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        Actions actions = new Actions(driver);
+
+        WebElement surveyTitle = driver.findElement(By.xpath("//div[@class='block block-poll']/div[1]"));
+        Assert.assertTrue("Anket bulunamadı", surveyTitle.isDisplayed());
+
+        WebElement goodOption = driver.findElement(By.xpath("//input[@id='pollanswers-2']"));
+        actions.moveToElement(goodOption).click().perform();
+
+        WebElement voteButton = driver.findElement(By.xpath("//input[@id='vote-poll-1']"));
+        actions.moveToElement(voteButton).click().perform();
+
+        Thread.sleep(2000);
+        WebElement warningMessage = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='block-poll-vote-error-1']")));
+        String messageActual = warningMessage.getText();
+
+        Assert.assertTrue("Uyarı mesajı görüntülenmedi",messageActual.contains("Only registered users can vote."));
+
+        System.out.println("Mesaj başarıyla görüntülendi: " + messageActual);
+
+        driver.quit();
+
+    }
+}
